dfac877efedacc916b2f021633e90dc7602c32e4
alv.penaloza <alv.penaloza@duocuc.cl>
2021-03-23 00:40:53 -0300

    nueva configuración

--- cypconsultores/settings.py
+++ cypconsultores/settings.py
@@ -86,19 +86,12 @@ WSGI_APPLICATION = 'cypconsultores.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 
-
 DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL')
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
 
 
 # Password validation
